11e2da9357361dfaaf08963ef4e0bd90051a529f
woaijssss <wenhanf@163.com>
2024-02-17 22:11:33 +0800

    update

--- constants/config_keys.go
+++ constants/config_keys.go
@@ -16,10 +16,16 @@ const (
 	LogLevel          = "app.log.level"
 	LogPath           = "app.log.path"
 
-	MysqlUrl         = "mysql.url"
-	MysqlPoolSize    = "mysql.poolSize"
-	MysqlMaxLife     = "mysql.maxLife"
+	// MysqlUrl 数据库url
+	MysqlUrl = "mysql.url"
+	// MysqlPoolSize 最大连接数
+	MysqlPoolSize = "mysql.poolSize"
+	// MysqlMaxLife 连接的最大生命周期，单位是秒
+	MysqlMaxLife = "mysql.maxLife"
+	// MysqlMaxIdleCons 最大空闲连接数
 	MysqlMaxIdleCons = "mysql.maxIdleCons"
+	// MysqlMaxIdleTime 最大空闲时间，单位是秒
 	MysqlMaxIdleTime = "mysql.maxIdleTime"
-	MysqlLog         = "mysql.log"
+	// MysqlLog 该在该数据源上执行sql是是否需要把待执行的sql输出到日志
+	MysqlLog = "mysql.log"
 )
