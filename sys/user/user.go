68baf635e15af1c5c758f8459ca075098ebbcb5d
woaijssss <wenhanf@163.com>
2025-04-13 10:39:02 +0800

    add MaskChineseName and MaskIDCard

--- sys/user/user.go
+++ sys/user/user.go
@@ -14,6 +14,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"unicode/utf8"
 )
 
 const (
@@ -115,4 +116,25 @@ func GetUserGender(gender string) int32 {
 		return 0
 	}
 	return int32(gdi)
+}
+
+// MaskChineseName Chinese names only retain the first character, and if the rest are two character names, they are replaced with an asterisk (*). If it is a name of 3 characters or more, replace it with two *'s
+func MaskChineseName(name string) string {
+	if len(name) == 0 {
+		return ""
+	}
+	firstRune, _ := utf8.DecodeRuneInString(name)
+	runeCount := utf8.RuneCountInString(name)
+	if runeCount == 2 {
+		return fmt.Sprintf("%c*", firstRune)
+	}
+	return fmt.Sprintf("%c**", firstRune)
+}
+
+// MaskIDCard The 11 digits in the middle of the second generation ID card number are replaced by *
+func MaskIDCard(idCard string) string {
+	if len(idCard) != 18 {
+		return idCard
+	}
+	return idCard[:3] + "********" + idCard[14:]
 }
